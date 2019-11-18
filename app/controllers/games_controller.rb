76a7f12dd19529c891b89f611cb1773bfc4c685b
Matlions <matthieurpeyronnet@gmail.com>
2019-11-18 14:59:50 +0100

    crazy longest word game

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,11 +1,41 @@
+require 'open-uri'
+require 'nokogiri'
+
 class GamesController < ApplicationController
   def new
-    # @letters = Array.new(10) { [*'A'..'Z'].sample }.join
-    @letters = Array.new(10).map { || ('A'..'Z').to_a.sample }.join
+    @array = operation_score
+     session[:count] = 0
   end
 
   def score
-    @word = params[:word]
+    array = params[:array].downcase
+    @created_word = params[:word]
+    session[:count] = @created_word.split('').count
+    @calcul = session[:count] * session[:count]
+    @user = {}
+    url = "https://wagon-dictionary.herokuapp.com/#{@created_word}"
+    response = open(url).read
+    @data = JSON.parse(response)
+    @exist = true
+    @data["word"].split('').each do |letter|
+      @exist = false unless array.include?(letter)
+    end
+    if @exist == true && @data["found"] == true
+      @hit = "Conguratulations!#{@created_word} is an English word!"
+    elsif @exist == false
+      @hit = "Sorry #{@created_word} can't be build of #{array}"
+    else
+      @rhit = "Sorry but #{@created_word} does not seem like an English word..."
+    end
+  end
 
+  private
+
+  def operation_score
+    (0..10).map { ('A'..'Z').to_a[rand(26)] }
+  end
+
+  def create
+    session[:user_id] = "m/ueqGM7N8x8wJSEEAvWI8T/60dlGKH9Ad0NkpB7mgRVb2PlzQUh5kIU76yG7kJW1P/uBGKB7IOC/w/sMBIwxw=="
   end
 end
